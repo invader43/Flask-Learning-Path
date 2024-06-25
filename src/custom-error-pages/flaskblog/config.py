1dabadd25d677b543b7a8783783f1833f3523e02
invader43# <55555dakkale@gmail.com>
2024-06-25 21:04:14 +0530

    edited config file

--- src/custom-error-pages/flaskblog/config.py
+++ src/custom-error-pages/flaskblog/config.py
@@ -1,13 +1,18 @@
 import os
+import json
+
+with open("/etc/config.json","r") as config_file:
+    config = json.load(config_file)
+    print(config)
 
 
 
 class Config:
-    SECRET_KEY = '03c846950425a22fc22fb6be5f12ef6d'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    MAIL_SERVER = 'smtp.gmail.com'
+    SECRET_KEY = config.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
+    MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 465
     MAIL_USER_TLS = False
     MAIL_USER_SSL = True
-    MAIL_USERNAME = os.environ.get('EMAIL')
-    MAIL_PASSWORD = os.environ.get('PASSWORD')
+    MAIL_USERNAME = config.get("EMAIL")
+    MAIL_PASSWORD = config.get("PASSWORD")
